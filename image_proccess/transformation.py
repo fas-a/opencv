1473589f275106fa26bd75bb6769b6a12637327f
iamPanda4 <abulkhoirfaris0@gmail.com>
2024-01-25 05:35:14 +0700

    image transformation

---
+++ image_proccess/transformation.py
@@ -0,0 +1,26 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('./image_proccess/messi5.jpg')
+scale = cv.resize(img, None, fx=2, fy=2, interpolation=cv.INTER_CUBIC)
+rows, cols, ch = img.shape
+M = np.float32([[1, 0, 100], [0, 1, 50]])
+translation = cv.warpAffine(img, M, (cols, rows))
+M = cv.getRotationMatrix2D((cols/2, rows/2), 90, 1)
+rotation = cv.warpAffine(img, M, (cols, rows))
+pts1 = np.float32([[50, 50], [200, 50], [50, 200]])
+pts2 = np.float32([[10, 100], [200, 50], [100, 250]])
+M = cv.getAffineTransform(pts1, pts2)
+affine = cv.warpAffine(img, M, (cols, rows))
+pts1 = np.float32([[56, 65], [238, 52], [28, 237], [239, 240]])
+pts2 = np.float32([[0, 0], [200, 0], [0, 200], [200, 200]])
+M = cv.getPerspectiveTransform(pts1, pts2)
+perspective = cv.warpPerspective(img, M, (200, 200))
+cv.imshow('perspective', perspective)
+cv.imshow('affine', affine)
+cv.imshow('rotation', rotation)
+cv.imshow('dst', translation)
+cv.imshow('img', img)
+cv.imshow('scale', scale)
+cv.waitKey(0)
+cv.destroyAllWindows()
